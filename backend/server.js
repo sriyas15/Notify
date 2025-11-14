1da9363956144d8fb46b867f1e28e967a6c0bb32
Mohamed Riyas <sriyas1508@gmail.com>
2025-11-14 10:35:05 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,33 @@ import notesRoutes from './src/routes/notesRoutes.js';
 import { connectDB } from './src/config/db.js';
 import rateLimiter from "./src/middleware/rateLimiter.js";
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
 connectDB();
 
-app.use(cors());
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+if( process.env.NODE_ENV !== "production"){
+    app.use(cors());
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes",notesRoutes);
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 app.listen(process.env.PORT,(req,res)=>{
     console.log(`Server is Started at ${process.env.PORT}`);
